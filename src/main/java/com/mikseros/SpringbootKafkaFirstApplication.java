f02041d6e88b3868cb15c51e84864fea66638e5f
mikseros <mmadynski@gmail.com>
2022-08-31 21:51:03 +0200

    Initial commit

---
+++ src/main/java/com/mikseros/SpringbootKafkaFirstApplication.java
@@ -0,0 +1,13 @@
+package com.mikseros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaFirstApplication.class, args);
+	}
+
+}
